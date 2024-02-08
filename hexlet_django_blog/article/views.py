223ee16f8ad41231559421a25c2f7f3df13511d0
Costard86 <Costard86@gmail.com>
2024-02-08 16:21:16 +0700

    exercise 4

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -5,13 +5,12 @@ from django.views import View
 
 
 class IndexView(View):
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('<h1>Welcome to My Django Blog!</h1>')
-
+     def get(self, request, *args, **kwargs):
+         return HttpResponse('<h1>Welcome to My Django Blog!</h1>')
 
 
-#def index(request):
-#    return render(request, 'articles/index.html', context={
-#        'app_name': '"hexlet_django_blog"',
-#    })
+
+# def index(request, tags, article_id):
+#     return render(request, 'articles/index.html', context={
+#         'app_name': '"hexlet_django_blog"',
+#     })
