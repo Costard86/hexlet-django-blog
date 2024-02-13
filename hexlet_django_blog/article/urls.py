4bbb7069201489abfcb0fa30008a5528173b932a
Costard86 <Costard86@gmail.com>
2024-02-13 18:37:24 +0700

    add deleted article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from hexlet_django_blog.article import views
-from .views import IndexView, ArticleView, ArticleCommentsView, ArticleFormCreateView, ArticleFormEditView
+from .views import (IndexView, ArticleView, ArticleCommentsView,
+                    ArticleFormCreateView, ArticleFormEditView,
+                    ArticleFormDeleteView)
 
 
 urlpatterns = [
@@ -9,6 +11,7 @@ urlpatterns = [
 #    path('<int:id>/', ArticleView.as_view(), name='article'), через имя
     path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
     path('<int:id>/', ArticleView.as_view()),
+    path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete'),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
