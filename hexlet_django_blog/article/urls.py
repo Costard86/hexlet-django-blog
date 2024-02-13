fa95b1a529fb77b43438657b68292277d415d143
Costard86 <Costard86@gmail.com>
2024-02-13 13:28:52 +0700

    add form for add article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from hexlet_django_blog.article import views
-from .views import IndexView
+from .views import IndexView, ArticleView, ArticleCommentsView, ArticleFormCreateView
 
 
 urlpatterns = [
   #  path('', IndexView.as_view(), name='article_index'),
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='articles'),
+#    path('<int:id>/', ArticleView.as_view(), name='article'), через имя
+    path('<int:id>/', ArticleView.as_view()),
+    path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,16 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from hexlet_django_blog.article.models import Article
+from .forms import CommentArticleForm, ArticleForm
 from django.views import View
 
 
 
 class IndexView(View):
-     def get(self, request, *args, **kwargs):
-         return HttpResponse('<h1>Welcome to My Django Blog!</h1>')
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
 
 
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={
+            'article': article,
+        })
 
-# def index(request, tags, article_id):
-#     return render(request, 'articles/index.html', context={
-#         'app_name': '"hexlet_django_blog"',
-#     })
+class ArticleCommentsView(View):
+
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, id=kwargs['id'], article__id=kwargs['article_id'])
+
+        return render(request, 'articles/show.html', context={
+            'article': article,
+        })
+
+class CommentArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = CommentArticleForm() # Создаем экземпляр нашей формы
+        return render(request, 'comment.html', {'form': form}) # Передаем нашу форму в контексте
+    
+class ArticleFormCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():  # Если данные корректные, то сохраняем данные формы
+            form.save()
+            return redirect('articles')  # Редирект на указанный маршрут
+        # Если данные некорректные, то возвращаем человека обратно на страницу с заполненной формой
+        return render(request, 'articles/create.html', {'form': form})
