54f642d78d6ae9cc8609d69382ecfface50701a6
Costard86 <Costard86@gmail.com>
2024-02-13 18:14:01 +0700

    add form for update article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from hexlet_django_blog.article import views
-from .views import IndexView, ArticleView, ArticleCommentsView, ArticleFormCreateView
+from .views import IndexView, ArticleView, ArticleCommentsView, ArticleFormCreateView, ArticleFormEditView
 
 
 urlpatterns = [
   #  path('', IndexView.as_view(), name='article_index'),
     path('', IndexView.as_view(), name='articles'),
 #    path('<int:id>/', ArticleView.as_view(), name='article'), через имя
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
     path('<int:id>/', ArticleView.as_view()),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
